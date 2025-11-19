de5e472b89bef9fa9ddedf08e4aad15b5ecff668
amanraj953426 <amanraj953426@gmail.com>
2025-11-19 06:46:33 +0530

    adding new project (key press) js file

--- Projects/05_backgroundChanger/background2.js
+++ Projects/05_backgroundChanger/background2.js
@@ -33,3 +33,26 @@ const start = document.getElementById('start').addEventListener('click', startCh
 
 
 const stop = document.getElementById('stop').addEventListener('click', stopChangingColor);
+
+// keydown Event
+const insert = document.getElementById('insert');
+const keycode = document.querySelector('.keycode');
+
+window.addEventListener('keydown', function (e) {
+    keycode.innerHTML = `
+   
+        <table>
+            <tr>
+                <th>key</th>
+                <th>keycode</th>
+                <th>code</th>
+            </tr>
+            <tr>
+                <td>${e.key === ' ' ? 'space' : e.key}</td>
+                <td>${e.keyCode}</td>
+                <td>${e.code}</td>
+            </tr>
+        </table>
+    
+`;
+});
